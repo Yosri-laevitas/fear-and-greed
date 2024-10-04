444498dba8d5a87585a4c8d6bb742acf8fd7e5c2
YOSRI BEN HALIMA <yosri.benhalima@ept.ucar.tn>
2024-10-04 15:23:20 +0100

    commit changes to fetchers

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,23 @@
 from .formatting import json_to_csv
 from .scaling import z_score_normalize, min_max_scale
-from .data_fetchers import get_historical_perps_page, get_historical_perps, get_df_items, get_instruments_data
+from .data_fetchers import (
+    get_historical_perps_page,
+    get_historical_perps, 
+    get_df_items, 
+    get_instruments_data, 
+    get_historical_futures_page,
+    get_historical_futures,
+    get_historical_options_page,
+    get_historical_options
+)
 
-__all__ = ['json_to_csv', 'min_max_scale', 'z_score_normalize']
+__all__ = [
+           'json_to_csv', 
+           'min_max_scale', 
+           'z_score_normalize', 
+           'get_historical_perps',
+           'get_historical_futures',
+           'get_df_items',
+           'get_instruments_data', 
+           'get_historical_options'
+          ]
--- src/utils/data_fetchers.py
+++ src/utils/data_fetchers.py
@@ -32,7 +32,7 @@ def get_df_items(items: list) -> pd.DataFrame:
 
     df = pd.DataFrame(items)
     df['date'] = pd.to_datetime(df['date'], unit='ms')
-    pd.set_option("future.no_silent_downcasting", True)
+    #pd.set_option("future.no_silent_downcasting", True)
     df = df.replace({None: np.nan})
 
     return df
@@ -41,18 +41,55 @@ def get_df_items(items: list) -> pd.DataFrame:
 # Instruments Data Fetchers
 # ----------------------------------------------------------------
 
-def get_instruments_data() -> pd.DataFrame:
+def get_instruments_data(currency: Literal['BTC', 'ETH'] = None, type: Literal['perpetual', 'option', 'future'] = None) -> pd.DataFrame:
     """
     Fetches instrument data from the Laevitas API.
 
+    Parameters
+    ----------
+    currency : Literal['BTC', 'ETH']
+        The currency of the instruments to retrieve. Default is None.
+    type : Literal['perpetual', 'option', 'future']
+        The type of instruments to retrieve. Default is None.
+
     Returns
     -------
-    dict
-        The JSON response containing the instrument data.
+    pd.DataFrame
+        A DataFrame containing the 'market' and 'instrument' columns of the filtered instrument data.
     """
-    url = base_url + '/analytics/futures/instruments'
-    response = requests.get(url=url, headers=headers)
-    return pd.DataFrame(response.json()['data'])
+    
+    url = f"{base_url}/analytics/futures/instruments"
+    target_cols  = ['market', 'instrument'] 
+    
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json().get('data', [])
+        
+        if not data:
+            raise ValueError("No data returned from API.")
+        
+        df = pd.DataFrame(data)
+        filtered_df = df.copy()
+        
+        if currency is not None: 
+            filtered_df = filtered_df[filtered_df['currency'] == currency]
+        if type is not None:
+            filtered_df = filtered_df[filtered_df['type'] == type]
+
+        if currency is None:
+            target_cols += ['currency']
+        if type is None:
+            target_cols += ['type']
+        
+        return filtered_df[target_cols].drop_duplicates().reset_index(drop=True)
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred while fetching data: {e}")
+        return pd.DataFrame()
+    except ValueError as ve:
+        print(ve)
+        return pd.DataFrame()
 
 # ----------------------------------------------------------------
 # Perps Data Fetchers
@@ -67,7 +104,7 @@ def get_historical_perps_page(market: str,
                               page: int = 1
                              ) -> dict:
     """
-    Fetches historical data for perpetual swaps from the Laevitas API per page.
+    Fetches historical data for a specified perpetual from the Laevitas API per page.
 
     Parameters
     ----------
@@ -91,7 +128,7 @@ def get_historical_perps_page(market: str,
     Returns
     -------
     dict 
-        A json containing a page of the historical data for the specified perpetual swap.
+        A json containing a page of the historical data for the specified perpetual.
     """
 
     params = {
@@ -114,9 +151,9 @@ def get_historical_perps(market: str,
                          end: str, 
                          granularity: Literal['5m', '15m', '30m','1h', '2h', '4h', '6h', '12h', '1d'], 
                          limit: int = 144
-                        ) -> list:
+                        ) -> pd.DataFrame:
     """
-    Fetches historical data for perpetual swaps from the Laevitas API.
+    Fetches historical data for a specified perpetual from the Laevitas API.
 
     Parameters
     ----------
@@ -137,8 +174,8 @@ def get_historical_perps(market: str,
     
     Returns
     -------
-    list
-        A list containing all the historical data points for the specified perpetual swap.
+    pd.DataFrame
+        A dataframe containing all the historical data points for the specified perpetual.
     """
 
     historical_data = get_historical_perps_page(market, symbol, start, end, granularity, limit)
@@ -149,7 +186,7 @@ def get_historical_perps(market: str,
         historical_data = get_historical_perps_page(market, symbol, start, end, granularity, limit, page)
         items += historical_data['items']
     
-    return items
+    return get_df_items(items)
 
 # ----------------------------------------------------------------
 # Futures Data Fetchers
@@ -157,7 +194,7 @@ def get_historical_perps(market: str,
 
 def get_historical_futures_page(market: str, symbol: str, start: str, end: str, granularity: Literal['1m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '12h', '1d'], limit: int = 144, page: int = 1) -> dict:
     """
-    Fetches historical data for futures contracts from the Laevitas API per page.
+    Fetches historical data for a specified futures contract from the Laevitas API per page.
 
     Parameters
     ----------
@@ -192,14 +229,149 @@ def get_historical_futures_page(market: str, symbol: str, start: str, end: str, 
         'page': page
     }
     
-    url = f'https://api.laevitas.ch/historical/derivs/futures/{market}/{symbol}'
+    url = base_url + f'/historical/derivs/futures/{market}/{symbol}'
     
     response = requests.get(url=url, params=params, headers=headers)
     
     return response.json()
 
+def get_historical_futures(market: str, 
+                           symbol: str, 
+                           start: str,
+                           end: str, 
+                           granularity: Literal['5m', '15m', '30m','1h', '2h', '4h', '6h', '12h', '1d'], 
+                           limit: int = 144
+                          ) -> pd.DataFrame:
+    """
+    Fetches historical data for a specified futures contract from the Laevitas API.
+
+    Parameters
+    ----------
+    market : str
+        The market for which the data is required.
+    symbol : str 
+        The symbol of the asset for which the data is required.
+    token : str 
+        The API token for authentication.
+    start : str 
+        The start date for the data in 'YYYY-MM-DD' format.
+    end : str 
+        The end date for the data in 'YYYY-MM-DD' format.
+    granularity : str 
+        The time interval for the data. Options: 5m, 15m, 30m, 1h, 2h, 4h, 6h, 12h, 1d.
+    limit : int, optional 
+        The maximum number of data points per page to retrieve. Default is 144.
+    
+    Returns
+    -------
+    pd.DataFrame
+        A dataframe containing all the historical data points for the specified futures.
+    """
+
+    historical_data = get_historical_futures_page(market, symbol, start, end, granularity, limit)
+    items = historical_data['items']
+    pages = historical_data['meta']['total_pages']
+
+    for page in range(2, pages + 1):
+        historical_data = get_historical_futures_page(market, symbol, start, end, granularity, limit, page)
+        items += historical_data['items']
+    
+    return get_df_items(items)
 
 # ----------------------------------------------------------------
 # Options Data Fetchers
 # ----------------------------------------------------------------
 
+def get_historical_options_page(market: str, 
+                                instrument: str, 
+                                start: str, 
+                                end: str, 
+                                granularity: Literal['5m', '15m', '30m','1h', '2h', '4h', '6h', '12h', '1d'], 
+                                limit: int = 144, 
+                                page: int = 1
+                               ) -> dict:
+    """
+    Fetches historical data for a specified options contract from the Laevitas API per page.
+
+    Parameters
+    ----------
+    market : str
+        The market for which the data is required.
+    symbol : str 
+        The symbol of the asset for which the data is required.
+    token : str 
+        The API token for authentication.
+    start : str 
+        The start date for the data in 'YYYY-MM-DD' format.
+    end : str 
+        The end date for the data in 'YYYY-MM-DD' format.
+    granularity : str
+        The time interval for the data. Options: 1m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 12h, 1d.
+    limit : int, optional
+        The maximum number of data points per page to retrieve. Default is 144.
+    page : int, optional
+        The page number for pagination. Default is 1.
+    
+    Returns
+    -------
+    dict
+        A json containing a page of the historical data for the specified options contract.
+    """
+
+    params = {
+        'start': start,
+        'end': end,
+        'granularity': granularity,
+        'limit': limit,
+        'page': page
+    }
+    
+    url = f'{base_url}/historical/options/{market}/{instrument}'
+    
+    response = requests.get(url=url, params=params, headers=headers)
+    
+    return response.json()
+
+def get_historical_options(market: str, 
+                           instrument: str, 
+                           start: str, 
+                           end: str,
+                           granularity: Literal['5m', '15m', '30m','1h', '2h', '4h', '6h', '12h', '1d'], 
+                           limit: int = 144
+                           ) -> pd.DataFrame:
+    """
+    Fetches historical data for options from the Laevitas API.
+
+    Parameters
+    ----------
+    market : str
+        The market for which the data is required.
+    instrument : str 
+        The symbol of the asset for which the data is required.
+    token : str 
+        The API token for authentication.
+    start : str 
+        The start date for the data in 'YYYY-MM-DD' format.
+    end : str 
+        The end date for the data in 'YYYY-MM-DD' format.
+    granularity : str 
+        The time interval for the data. Options: 5m, 15m, 30m, 1h, 2h, 4h, 6h, 12h, 1d.
+    limit : int, optional 
+        The maximum number of data points per page to retrieve. Default is 144.
+    
+    Returns
+    -------
+    pd.DataFrame
+        A dataframe containing all the historical data points for the specified options.
+    """
+    
+    historical_data = get_historical_options_page(market, instrument, start, end, granularity, limit)
+    items = historical_data['items']
+    total_items = historical_data['meta']['total']
+    pages = int(np.ceil(total_items / limit))
+    
+    for page in range(2, pages + 1):
+        historical_data = get_historical_options_page(market, instrument, start, end, granularity, limit, page)
+        items += historical_data['items']
+    
+    return get_df_items(items)
