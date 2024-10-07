ab2faaa8e02f4c239780bb0eb8ff09d511687f9f
Yosri Ben Halima <yosri@laevitas.ch>
2024-10-07 09:47:22 +0100

    Push

--- src/marketdata/crypto_market_data.py
+++ src/marketdata/crypto_market_data.py
@@ -2,7 +2,51 @@ from typing import Literal
 from src.utils import get_instruments_data
 
 class CryptoMarketData():
+    """
+    A class to represent cryptocurrency market data.
+
+    Attributes
+    ----------
+    currency : Literal["BTC", "ETH"]
+        The cryptocurrency symbol.
+    type : Literal['perpetual', 'option', 'future']
+        The type of the instrument.
+    start : str
+        The start date for the data in 'YYYY-MM-DD' format.
+    end : str
+        The end date for the data in 'YYYY-MM-DD' format.
+    granularity : Literal['5m', '15m', '30m','1h', '2h', '4h', '6h', '12h', '1d']
+        The time interval for the data.
+
+    Properties
+    ----------
+    currency
+        Getter and setter for the currency.
+    start
+        Getter and setter for the start date.
+    end
+        Getter and setter for the end date.
+    granularity
+        Getter and setter for the granularity.
+    """
+
     def __init__(self, currency: Literal["BTC", "ETH"], type: Literal['perpetual', 'option', 'future'], start: str, end: str, granularity: Literal['5m', '15m', '30m','1h', '2h', '4h', '6h', '12h', '1d']) -> None:
+        """
+        Initializes a new instance of CryptoMarketData.
+
+        Parameters
+        ----------
+        currency : Literal["BTC", "ETH"]
+            The cryptocurrency symbol.
+        type : Literal['perpetual', 'option', 'future']
+            The type of the instrument.
+        start : str
+            The start date for the data in 'YYYY-MM-DD' format.
+        end : str
+            The end date for the data in 'YYYY-MM-DD' format.
+        granularity : Literal['5m', '15m', '30m','1h', '2h', '4h', '6h', '12h', '1d']
+            The time interval for the data.
+        """
         self.__currency = currency
         self.__start = start
         self.__end = end
