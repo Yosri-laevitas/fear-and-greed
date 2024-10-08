82db368a8098abc2f889128b651ca95bdb376e25
Yosri Ben Halima <yosri@laevitas.ch>
2024-10-08 14:49:57 +0100

    Commit

--- services/mongodb_service.py
+++ services/mongodb_service.py
@@ -1,20 +1,42 @@
 from pymongo import MongoClient
-from typing import Literal
+from typing import Literal, Optional
+from datetime import datetime
 
 type = "futures"
 coin = "btc"
 
 
 def get_futures(
-    coin: Literal["BTC", "ETH"], type: Literal["futures", "options", "perpetuals"]
+    coin: Literal["BTC", "ETH"],
+    type: Literal["futures", "options", "perpetuals"],
+    start: Optional[str] = None,
+    end: Optional[str] = None,
+    limit: Optional[int] = None,
 ) -> list:
     client = MongoClient("mongodb://localhost:27017/")
     db = client["laevitas"]
     collection = db[type]
 
-    query = {"currency": {"$regex": coin}, "$expr": {"$eq": [{"$hour": "$date"}, 0]}}
+    # Create the base query for currency and hour
+    query = {
+        "currency": {"$regex": coin, "$options": "i"},
+        "$expr": {"$eq": [{"$hour": "$date"}, 0]},
+    }
+
+    # Add date range filter if start and end dates are provided
+    if start:
+        query["date"] = {
+            "$gte": datetime.fromisoformat(start)
+        }  # Assuming ISO format for input
+    if end:
+        query.setdefault("date", {})["$lte"] = datetime.fromisoformat(end)
+
     projection = {"currency": 1, "date": 1, "points.0": 1}
-    results = collection.find(query, projection).limit(10)
+
+    if limit is not None:
+        results = collection.find(query, projection).limit(limit)
+    else:
+        results = collection.find(query, projection)
 
     client.close()
 
