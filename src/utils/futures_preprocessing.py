29120eb37f7acb13c6b819f38d161f7e70ae7f4a
Yosri Ben Halima <yosri@laevitas.ch>
2024-10-08 17:56:18 +0100

    preprocessing tools

---
+++ src/utils/futures_preprocessing.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+import re
+import pandas as pd
+
+
+def parse_expiry(currency):
+    """Parse currency into expiry date."""
+    pattern = r"(\d{1,2})([a-z]{3})(\d{2})"
+    match = re.search(pattern, currency)
+
+    if match:
+        day, month_str, year_str = match.groups()
+        day = int(day)
+        year = int(year_str) + 2000
+
+        month = datetime.strptime(month_str, "%b").month
+
+        expiry_date = datetime(year, month, day).strftime("%Y-%m-%d")
+        return expiry_date
+    else:
+        raise ValueError(f"Error parsing currency '{currency}': No match found")
+
+
+def add_expiry_column(df: pd.DataFrame) -> pd.DataFrame:
+    """Add expiry column to df DataFrame."""
+    df["expiry"] = df["currency"].apply(parse_expiry)
+    return df
+
+
+def calculate_days_to_expiry(df: pd.DataFrame) -> pd.DataFrame:
+    """Calculate days to expiry and add it to the DataFrame."""
+    df["date"] = pd.to_datetime(df["date"])
+    df["expiry"] = pd.to_datetime(df["expiry"])
+    df["days_to_expiry"] = (df["expiry"] - df["date"]).dt.days
+    return df
+
+
+def calculate_annualized_basis(df: pd.DataFrame) -> pd.DataFrame:
+    """Calculate annualized basis and add it to the DataFrame."""
+    df["annualized_basis"] = df["basis"] * (365 / df["days_to_expiry"])
+    return df
+
+
+def filter_and_aggregate(df: pd.DataFrame) -> pd.DataFrame:
+    """Filter and group the df DataFrame."""
+    df = df[~(df["days_to_expiry"] == 0)]
+    df = df.groupby("date").mean(numeric_only=True).round(2)
+    return df
+
+
+def process_futures(futures: pd.DataFrame) -> pd.DataFrame:
+    """Process the futures DataFrame."""
+    df = futures.copy()
+    df = add_expiry_column(df)
+    df = calculate_days_to_expiry(df)
+    df = calculate_annualized_basis(df)
+    df = filter_and_aggregate(df)
+    return df[["price", "annualized_basis", "open_interest", "volume"]]
